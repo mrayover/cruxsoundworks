44b9abab404ba247aed69e4fc45c863b2d5f72e5
mrayover <mroveracker@gmail.com>
2025-06-28 15:19:18 -0700

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,32 +2,37 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className="bg-dark text-neutral p-4 flex flex-col sm:flex-row items-center justify-between">
-      <div className="flex items-center space-x-2">
-        <img src="/discretus-glyph.png" alt="Discretus Glyph" className="h-10" />
-      </div>
-      <nav className="flex space-x-4 mt-2 sm:mt-0">
-        <a href="/" className="hover:opacity-70 font-bold">Home</a>
-        <div className="relative group">
-  <span className="hover:opacity-70 font-bold cursor-pointer">Works</span>
-  <div className="absolute hidden group-hover:block bg-dark text-neutral mt-2 py-2 px-4 rounded shadow-lg z-50">
-    <a href="/works/tarot-tree-of-life" className="block py-1 hover:underline">Tarot Tree of Life</a>
-    <a href="/works/to-autumn" className="block py-1 hover:underline">To Autumn</a>
-    <a href="/works/unearthing-home" className="block py-1 hover:underline">Unearthing Home</a>
-    <a href="/works/counting-seconds" className="block py-1 hover:underline">Counting Seconds</a>
-    <a href="/works/corrida" className="block py-1 hover:underline">Corrida</a>
-    <a href="/works/grafan-imago" className="block py-1 hover:underline">Grafan Imago</a>
-    <a href="/works/string-quartet" className="block py-1 hover:underline">String Quartet</a>
-    <a href="/works/discretus-discernere" className="block py-1 hover:underline">Discretus & Discernere</a>
-    <a href="/works/seven-against-thebes" className="block py-1 hover:underline">Seven Against Thebes</a>
-    <a href="/works/counting-down" className="block py-1 hover:underline">Counting Down</a>
-    <a href="/works" className="block py-1 mt-2 pt-2 border-t border-neutral/30 hover:underline">All Works</a>
+<header className="bg-dark text-neutral px-4 py-6 flex items-start justify-between flex-wrap">
+  {/* Left: Glyph + Home */}
+  <div className="flex flex-col items-start">
+    <a href="/" className="block">
+      <img src="/discretus-glyph.png" alt="Discretus Glyph" className="h-24" />
+    </a>
+    <a href="/" className="mt-1 text-sm font-semibold hover:underline">Home</a>
   </div>
-</div>
-        <a href="/lessons" className="hover:opacity-70 font-bold">Lessons</a>
-        <a href="/contact" className="hover:opacity-70 font-bold">Contact</a>
-      </nav>
-    </header>
+
+  {/* Right: Nav links */}
+  <nav className="flex-1 flex justify-evenly mt-4 sm:mt-0 sm:justify-end gap-4 text-lg">
+    <div className="relative group">
+      <span className="hover:opacity-70 font-bold cursor-pointer">Works</span>
+      <div className="absolute opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition-opacity duration-200 bg-dark text-neutral mt-2 py-2 px-4 rounded shadow-lg z-50">
+        <a href="/works/tarot-tree-of-life" className="block py-1 hover:underline">Tarot Tree of Life</a>
+        <a href="/works/to-autumn" className="block py-1 hover:underline">To Autumn</a>
+        <a href="/works/unearthing-home" className="block py-1 hover:underline">Unearthing Home</a>
+        <a href="/works/counting-seconds" className="block py-1 hover:underline">Counting Seconds</a>
+        <a href="/works/corrida" className="block py-1 hover:underline">Corrida</a>
+        <a href="/works/grafan-imago" className="block py-1 hover:underline">Grafan Imago</a>
+        <a href="/works/string-quartet" className="block py-1 hover:underline">String Quartet</a>
+        <a href="/works/discretus-discernere" className="block py-1 hover:underline">Discretus & Discernere</a>
+        <a href="/works/seven-against-thebes" className="block py-1 hover:underline">Seven Against Thebes</a>
+        <a href="/works/counting-down" className="block py-1 hover:underline">Counting Down</a>
+        <a href="/works" className="block py-1 mt-2 pt-2 border-t border-neutral/30 hover:underline">All Works</a>
+      </div>
+    </div>
+    <a href="/lessons" className="hover:opacity-70 font-bold">Lessons</a>
+    <a href="/contact" className="hover:opacity-70 font-bold">Contact</a>
+  </nav>
+</header>
   )
 }
 
