49b4e236de15ec5787ef36d27d5a9b5cd58f3e69
mrayover <mroveracker@gmail.com>
2025-06-28 14:07:27 -0700

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import Layout from './components/Layout'
 
 function App() {
-  return (
-    <Layout>
-      <div className="text-center py-8">
-        <h1 className="text-4xl font-bold">Welcome to Crux Soundworks</h1>
-        <p className="text-lg mt-2 italic">The Pattern Beneath the Noise</p>
-      </div>
-    </Layout>
-  )
+ return (
+  <div className="text-center py-8">
+    <h1 className="text-4xl font-bold">Welcome to Crux Soundworks</h1>
+    <p className="text-lg mt-2 italic">The Pattern Beneath the Noise</p>
+  </div>
+);
 }
 
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,22 @@ const Header = () => {
       </div>
       <nav className="flex space-x-4 mt-2 sm:mt-0">
         <a href="/" className="hover:opacity-70 font-bold">Home</a>
-        <a href="/works" className="hover:opacity-70 font-bold">Works</a>
+        <div className="relative group">
+  <span className="hover:opacity-70 font-bold cursor-pointer">Works</span>
+  <div className="absolute hidden group-hover:block bg-dark text-neutral mt-2 py-2 px-4 rounded shadow-lg z-50">
+    <a href="/works/tarot-tree-of-life" className="block py-1 hover:underline">Tarot Tree of Life</a>
+    <a href="/works/to-autumn" className="block py-1 hover:underline">To Autumn</a>
+    <a href="/works/unearthing-home" className="block py-1 hover:underline">Unearthing Home</a>
+    <a href="/works/counting-seconds" className="block py-1 hover:underline">Counting Seconds</a>
+    <a href="/works/corrida" className="block py-1 hover:underline">Corrida</a>
+    <a href="/works/grafan-imago" className="block py-1 hover:underline">Grafan Imago</a>
+    <a href="/works/string-quartet" className="block py-1 hover:underline">String Quartet</a>
+    <a href="/works/discretus-discernere" className="block py-1 hover:underline">Discretus & Discernere</a>
+    <a href="/works/seven-against-thebes" className="block py-1 hover:underline">Seven Against Thebes</a>
+    <a href="/works/counting-down" className="block py-1 hover:underline">Counting Down</a>
+    <a href="/works" className="block py-1 mt-2 pt-2 border-t border-neutral/30 hover:underline">All Works</a>
+  </div>
+</div>
         <a href="/lessons" className="hover:opacity-70 font-bold">Lessons</a>
         <a href="/contact" className="hover:opacity-70 font-bold">Contact</a>
       </nav>
