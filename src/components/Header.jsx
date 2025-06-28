0202b720a262bbfd8f58c3e2e1104d95878d71a6
mrayover <mroveracker@gmail.com>
2025-06-28 15:43:18 -0700

    rgrgr

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,8 @@ import React from 'react'
 const Header = () => {
   return (
 <header className="bg-dark text-neutral px-6 py-4 flex items-center justify-between">
-  {/* Left: Glyph + Home */}
-  <div className="flex items-center gap-4">
-    <a href="/">
-      <img src="/discretus-glyph.png" alt="Discretus Glyph" className="h-40" />
-    </a>
-    <a href="/" className="text-xl font-semibold hover:underline">Home</a>
-  </div>
-
-  {/* Center: Works + Lessons */}
-  <div className="flex gap-24 items-center text-2xl">
+  {/* Left Nav: Works */}
+  <div className="text-2xl">
     <div className="relative group">
       <span className="hover:opacity-70 font-bold cursor-pointer">Works</span>
       <div className="absolute opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition-opacity duration-200 bg-dark text-neutral mt-2 py-2 px-4 rounded shadow-lg z-50">
@@ -29,14 +21,19 @@ const Header = () => {
         <a href="/works" className="block py-1 mt-2 pt-2 border-t border-neutral/30 hover:underline">All Works</a>
       </div>
     </div>
-    <a href="/lessons" className="hover:opacity-70 font-bold">Lessons</a>
   </div>
 
-  {/* Right: Contact */}
+  {/* Center Glyph (clickable) */}
+  <a href="/">
+    <img src="/discretus-glyph.png" alt="Discretus Glyph" className="h-40" />
+  </a>
+
+  {/* Right Nav: Lessons */}
   <div className="text-2xl">
-    <a href="/contact" className="hover:opacity-70 font-bold">Contact</a>
+    <a href="/lessons" className="hover:opacity-70 font-bold">Lessons</a>
   </div>
 </header>
+
   )
 }
 
