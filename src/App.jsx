9effeae2ebf8a058b9d6e2444e396140f1e339dc
mrayover <mroveracker@gmail.com>
2025-06-28 19:49:15 -0700

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import Layout from './components/Layout';
 import Home from './Home';
 
 function App() {
-  return (
-    <Layout>
-      <Home />
-    </Layout>
-  );
+  return <Home />;
 }
 
 export default App;
-
