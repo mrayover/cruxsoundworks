eee5c1a956dccdf360988b459003b65bb4a1167d
mrayover <mroveracker@gmail.com>
2025-07-02 12:04:45 -0700

    asdfsd

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import SidePanel from './SidePanel';
 
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-<main className="flex-1 mx-auto p-4 flex gap-8 max-w-6xl">
-  <div className="w-3/4">{children}</div>
-  <aside className="w-1/4">
-    <SidePanel type="works" />
-  </aside>
-</main>
+      <main className="flex-1 max-w-4xl mx-auto p-4">
+        {children}
+      </main>
       <div className="w-full">
         <Footer />
       </div>
