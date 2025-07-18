289377518c3ebb9975cee93be5b80d0b0e7ccf06
mrayover <mroveracker@gmail.com>
2025-07-17 18:44:17 -0700

    adsfasdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import Home from './Home';
+import CruxWorks from './pages/CruxWorks/Index';
+import FresnoComposersSociety from './pages/FresnoComposersSociety/Index';
 
 function App() {
   return <Home />;
 }
+<Routes>
+  {/* other routes */}
+  <Route path="/cruxworks" element={<CruxWorks />} />
+  <Route path="/fresnocomposersociety" element={<FresnoComposersSociety />} />
+  <Route path="/fresno" element={<Navigate to="/fresnocomposersociety" />} />
+  <Route path="/fcs" element={<Navigate to="/fresnocomposersociety" />} />
+</Routes>
 
 export default App;
