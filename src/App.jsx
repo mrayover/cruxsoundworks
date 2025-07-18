2385748746bcfd94a01cc35a76ec00bba644b1f6
mrayover <mroveracker@gmail.com>
2025-07-17 18:54:11 -0700

    asdfasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import Home from './Home';
 import CruxWorks from './pages/CruxWorks/index';
 import FresnoComposersSociety from './pages/FresnoComposersSociety/index';
 
-function App() {
-  return <Home />;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cruxworks" element={<CruxWorks />} />
+      <Route path="/fresnocomposersociety" element={<FresnoComposersSociety />} />
+      {/* Optional aliases */}
+      <Route path="/fresno" element={<Navigate to="/fresnocomposersociety" />} />
+      <Route path="/fcs" element={<Navigate to="/fresnocomposersociety" />} />
+    </Routes>
+  );
 }
-<Routes>
-  {/* other routes */}
-  <Route path="/cruxworks" element={<CruxWorks />} />
-  <Route path="/fresnocomposersociety" element={<FresnoComposersSociety />} />
-  <Route path="/fresno" element={<Navigate to="/fresnocomposersociety" />} />
-  <Route path="/fcs" element={<Navigate to="/fresnocomposersociety" />} />
-</Routes>
 
-export default App;
