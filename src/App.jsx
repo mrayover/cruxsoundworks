f9424440031d42ee4fd81288990af775f00f9331
mrayover <mroveracker@gmail.com>
2025-07-17 18:57:42 -0700

    sghgfhs

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-
 import Home from './Home';
-import CruxWorks from './pages/CruxWorks/index';
-import FresnoComposersSociety from './pages/FresnoComposersSociety/index';
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/cruxworks" element={<CruxWorks />} />
-      <Route path="/fresnocomposersociety" element={<FresnoComposersSociety />} />
-      {/* Optional aliases */}
-      <Route path="/fresno" element={<Navigate to="/fresnocomposersociety" />} />
-      <Route path="/fcs" element={<Navigate to="/fresnocomposersociety" />} />
-    </Routes>
-  );
+  return <Home />;
 }
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,53 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import App from './App'
-import './index.css'
-import Layout from './components/Layout'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+import Layout from './components/Layout';
 import WorksIndex from './pages/WorksIndex';
 import WorkDetail from './pages/WorkDetail';
 import AdminAddWork from './pages/AdminAddWork';
 import Calendar from './pages/Calendar';
 
-import Login from './pages/admin/Login'
-import ComposerDashboard from './pages/admin/ComposerDashboard'
-import ProtectedRoute from './components/ProtectedRoute'
-import { AuthProvider } from './context/AuthContext';
+import Login from './pages/admin/Login';
+import ComposerDashboard from './pages/admin/ComposerDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 import AdminIndex from './pages/admin/AdminIndex';
 
-// ...import other works as needed
+import CruxWorks from './pages/CruxWorks/Index';
+import FresnoComposersSociety from './pages/FresnoComposersSociety/Index';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/calendar" element={<Calendar />} />
-            <Route path="/" element={<App />} />
-            <Route path="/works" element={<WorksIndex />} />
-            <Route path="/works/:slug" element={<WorkDetail />} />
-            <Route path="/admin/add-work" element={<AdminAddWork />} />
-            <Route path="/admin" element={<AdminIndex />} />
-            {/* 🔐 Admin Composer Routes */}
-            <Route path="/admin/composer/login" element={<Login />} />
-            <Route
-              path="/admin/composer"
-              element={
-                <ProtectedRoute>
-                  <ComposerDashboard />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </Layout>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/works" element={<WorksIndex />} />
+          <Route path="/works/:slug" element={<WorkDetail />} />
+          <Route path="/calendar" element={<Calendar />} />
+
+          {/* Admin */}
+          <Route path="/admin/add-work" element={<AdminAddWork />} />
+          <Route path="/admin" element={<AdminIndex />} />
+          <Route path="/admin/composer/login" element={<Login />} />
+          <Route
+            path="/admin/composer"
+            element={
+              <ProtectedRoute>
+                <ComposerDashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* NEW ROUTES */}
+          <Route path="/cruxworks" element={<CruxWorks />} />
+          <Route path="/fresnocomposersociety" element={<FresnoComposersSociety />} />
+          <Route path="/fresno" element={<Navigate to="/fresnocomposersociety" />} />
+          <Route path="/fcs" element={<Navigate to="/fresnocomposersociety" />} />
+        </Routes>
+      </Layout>
+    </Router>
   </React.StrictMode>
-)
+);
