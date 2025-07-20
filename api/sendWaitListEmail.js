6ac28b753af782e62e3026b1123fb3114eb1fc40
mrayover <mroveracker@gmail.com>
2025-07-20 14:05:09 -0700

    asdfwefsf

--- api/sendWaitListEmail.js
+++ api/sendWaitListEmail.js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
       `,
     });
 
+    
     return res.status(200).json({ success: true });
   } catch (err) {
     console.error('Email send failed:', err);
